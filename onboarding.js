8071ea592446e85563443165c7861c45842e5a1b
MovingMelody <saisumanth2303@gmail.com>
2021-06-21 17:16:26 +0530

    rewriting the  onboarding logic to using the ninjs techniwque

--- onboarding.js
+++ onboarding.js
@@ -1,12 +1,5 @@
-hey i am onboarding feature and the things is that you have to 
-slkdjlsdjf
-gskjfhdskfj
-
-
-lorem ipsum and thing is i am currently working on this fuck so you have to reaplce the  main fiafgopasfgoasdfoasdfhjioasdjhfa
-aisdjhfkasdjfhakdsfhaksdfhasdhfsumanth ans this is not the end\\\
-end of the world so becarefula to be the answarable and this is import so in for const let 
-
-
-
-of of import this this this this this this this shit this this shit shit thit
+rewritng the whole onboarding logic here so be carefuil the upstream has the previous onboarding logic 
+you as
+new
+new
+new onboarding logic here so be careful
